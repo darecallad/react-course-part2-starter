bb32c2dbcab939b0a8309c17432a89af8ff04285
Johnson <Ashane199080706@hotmail.com.tw>
2023-10-19 16:34:15 -0700

    useInfiniteQuery to create infinite page

--- src/react-query/PostList.tsx
+++ src/react-query/PostList.tsx
@@ -1,33 +1,37 @@
-import { useState } from "react";
+import React from "react";
 import usePosts from "../hooks/usePosts";
 
 const PostList = () => {
   const pageSize = 10;
-  const [page, setPage] = useState(1);
-  const { data: posts, error, isLoading } = usePosts({ page, pageSize });
+
+  const {
+    data: posts,
+    error,
+    isLoading,
+    fetchNextPage,
+    isFetchingNextPage,
+  } = usePosts({ pageSize });
   if (error) return <p>{error.message}</p>;
   if (isLoading) return <p>Loading...</p>;
   return (
     <>
       <ul className="list-group">
-        {posts?.map((post) => (
-          <li key={post.id} className="list-group-item">
-            {post.title}
-          </li>
+        {posts.pages.map((page, index) => (
+          <React.Fragment key={index}>
+            {page.map((post) => (
+              <li key={post.id} className="list-group-item">
+                {post.title}
+              </li>
+            ))}
+          </React.Fragment>
         ))}
       </ul>
+
       <button
-        onClick={() => setPage(page - 1)}
-        disabled={page === 1}
-        className="btn btn-primary my-3"
-      >
-        Previous
-      </button>
-      <button
-        onClick={() => setPage(page + 1)}
+        onClick={() => fetchNextPage()}
         className="btn btn-primary my-3 ms-1"
       >
-        Next
+        {isFetchingNextPage ? "Loading..." : "Load More"}
       </button>
     </>
   );
