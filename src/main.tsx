4eccddd1305e158b8dc81482a650b43062a3f92c
Johnson <Ashane199080706@hotmail.com.tw>
2023-10-18 22:51:20 -0700

    set up react-query

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+import "bootstrap/dist/css/bootstrap.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-).render(
+const query = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={query}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
